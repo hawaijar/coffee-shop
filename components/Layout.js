b90c632fc0be951826eae4bef7b3d808329cef20
hawaijar <hawaijar.js@gmail.com>
2021-11-06 14:50:40 +0530

    Added mui/styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Head from "next/head";
 import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import useStyles from "../utils/styles";
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>My Barista</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={"bg-gray-700 cursor-pointer "}>
         <Toolbar>
           <Typography>My Starbucks</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
+      <Container className={classes.container}>{children}</Container>
       <footer>
         <Typography>All rights reserved. My starbucks</Typography>
       </footer>
---
+++ utils/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  container: {
+    minHeight: "80vh",
+  },
+});
+
+export default useStyles;
